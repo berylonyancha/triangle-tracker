43a826b38f618495eb924072dfef01517e9aad69
Beryl Onyancha <berylonyancha@gmail>
2019-04-28 21:47:00 +0300

    Introduce variables

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+
+  var data1=document.getElementById("sideA").value;
+  var data2=document.getElementById("sideB").value;
+  var data3=document.getElementById("sideC").value;
+  var sidea=parseInt(data1);
+  var sideb=parseInt(data2);
+  var sidec=parseInt(dat3);
+}
