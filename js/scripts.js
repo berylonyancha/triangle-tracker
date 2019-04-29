5613e36c84851588c5c8aff04ca76d0def1f1a66
Beryl Onyancha <berylonyancha@gmail>
2019-04-29 05:45:44 +0300

    delete folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function myFunction() {
+var myFunction =function(sidea, sideb, side c) {
 
 
 
@@ -8,26 +8,18 @@ function myFunction() {
 
   var sides=[sidea,sideb,sidec];
   var type=["equilateral triangle","isosceles triangle","scalene triangle","not a triangle"]
-}
+ 
 
-var sides = [];
-sides.push(sidea);
-sides.push(sideb);
-sides.push(sidec);
 
-// if (side[0]<0 || side[1]<0 || side[2]<0){
-//   var output1=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
-//   document.getElementById("triangleTracker").innerHTML=output1;
-// }
-
-if (
+ if 
+    (
         sidea + sideb > sidec &&
         sideb + sidec > sidea &&
         sidea + sidec> sideb &&
         (sidea === sideb && sideb === sidec);
     ) {
 
-        document.getElementById("output").innerHTML =
+        output.textcontent =
         "Equilateral Triangle";
     } else if (
         sidea + sideb > sidec &&
@@ -38,7 +30,7 @@ if (
         (sidea === sidec && sidec !== sideb) ||
         (sideb === sidec && sideb !== sidea))
     ) {
-        document.getElementById("output").innerHTML =
+        output.textcontent =
         "Isosceles Triangle";
 
 
@@ -49,5 +41,8 @@ if (
         sideb + sidec > sidea &&
         (sidea !== sideb && sideb !== sidec &&
         sidea !== sidec)
-        document.getElementById("output").innerHTML =
+        output.textcontent=
         "Scalene Triangle";
+
+    };
+};
